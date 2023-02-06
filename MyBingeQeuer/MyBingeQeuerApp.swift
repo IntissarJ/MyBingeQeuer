0c25ab3a33c78ab16aa435186f7fb1c5a827dc41
IntissarJ <intissar_jamal@outlook.com>
2023-02-06 14:08:44 +0100

    Initial Commit

---
+++ MyBingeQeuer/MyBingeQeuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBingeQeuerApp.swift
+//  MyBingeQeuer
+//
+//  Created by Intissar Jamal on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyBingeQeuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
